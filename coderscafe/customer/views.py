f07471c4c12fdfb39f8df125b4bcadc80d92509b
stokeydev <bbektas.dev@gmail.com>
2023-03-21 16:40:58 +0000

    still need to fix views py

--- coderscafe/customer/views.py
+++ coderscafe/customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from .models import MenuItem, Category, OrderModel
 
 # Create your views here.
 
@@ -14,9 +15,53 @@ class About(View):
 
 class Order(View):
     def get(self, request, *args, **kwargs):
-        pass
         #get every item from each category
+        starters = MenuItem.objects.filter(category__name__contains='Starter')
+        mains = MenuItem.objects.filter(category__name__contains='Main')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drinks')
 
         #pass into context
-
+        context = {
+            'starters': starters,
+            'mains': mains,
+            'desserts': desserts,
+            'drinks': drinks,
+        }
+        
         #render the template
+        return render(request, 'customer/order.html', context)
+
+    def post(self, request, *args, **kwargs):
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price
+            }
+
+            order_items['items'].append(item_data)
+
+            price = 0
+            item_ids = []
+    
+            for item in order_items['items']:
+                price += item['price']
+                item_ids.append(item['id'])
+
+            
+            order = OrderModel.objects.create(price=price)
+            order.items.add(*item_ids)
+
+            context = {
+                'items': order_items['items'],
+                'price': price
+            }
+            return render(request, 'customer/order_confirmation.html', context)

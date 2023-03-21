8c254111d4c302d969f448dd26eed40c12dc0201
stokeydev <bbektas.dev@gmail.com>
2023-03-21 15:38:03 +0000

    adding items

--- coderscafe/customer/views.py
+++ coderscafe/customer/views.py
@@ -11,3 +11,12 @@ class Index(View):
 class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')        
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        pass
+        #get every item from each category
+
+        #pass into context
+
+        #render the template

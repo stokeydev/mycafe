ab7a1897b43f4442aae1715b2318b897fc337533
stokeydev <bbektas.dev@gmail.com>
2023-03-21 16:50:45 +0000

    fixed indent code

--- coderscafe/customer/views.py
+++ coderscafe/customer/views.py
@@ -52,16 +52,16 @@ class Order(View):
             price = 0
             item_ids = []
     
-            for item in order_items['items']:
-                price += item['price']
-                item_ids.append(item['id'])
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
 
-            
-            order = OrderModel.objects.create(price=price)
-            order.items.add(*item_ids)
+        
+        order = OrderModel.objects.create(price=price)
+        order.items.add(*item_ids)
 
-            context = {
-                'items': order_items['items'],
-                'price': price
-            }
-            return render(request, 'customer/order_confirmation.html', context)
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+        return render(request, 'customer/order_confirmation.html', context)

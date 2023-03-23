4d8110f9ef48721c57fd29ef91c150a8b09750e0
stokeydev <bbektas.dev@gmail.com>
2023-03-23 15:36:03 +0000

    paypal added

--- coderscafe/customer/models.py
+++ coderscafe/customer/models.py
@@ -26,6 +26,7 @@ class OrderModel(models.Model):
     city = models.CharField(max_length=50, blank=True)
     county = models.CharField(max_length=15, blank=True)
     post_code = models.CharField(max_length=15, blank=True, null=True)
+    is_paid = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
--- coderscafe/customer/views.py
+++ coderscafe/customer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.core.mail import send_mail
 from .models import MenuItem, Category, OrderModel
@@ -95,4 +95,24 @@ class Order(View):
             'price': price
         }
 
+        return redirect('order-confirmation', pk=order.pk)
+
+class OrderConfirmation(View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+
+        context = {
+        'pk': order.pk,
+        'items': order.items,
+        'prices': order.price,   
+        }
+
         return render(request, 'customer/order_confirmation.html', context)
+    
+    def post(self, request, pk, *args, **kwargs):
+        print(request.body)  
+        return redirect('payment-confirmation')   
+
+class OrderPayConfirmation(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/order_pay_confirmation.html')     

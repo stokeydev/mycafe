78275b31cef3d0ee5c2fff8b475c2e35ec638795
stokeydev <bbektas.dev@gmail.com>
2023-03-31 14:23:46 +0000

    unshipped option on dashboard

--- coderscafe/cafe/views.py
+++ coderscafe/cafe/views.py
@@ -12,14 +12,18 @@ class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
         orders = OrderModel.objects.filter(
             created_on__year=today.year, created_on__month=today.month, created_on__day=today.day)
    
-        #loop through the orders and add the price value
+        #loop through the orders and add the price value, check if order is not shipped
+        unshipped_orders = []
         total_revenue = 0
         for order in orders:
             total_revenue += order.price
 
+            if not order.is_shipped:
+                unshipped_orders.append(order)
+
         #pass total number of orders and total revenue into template
         context = {
-            'orders': orders,
+            'orders': unshipped_orders,
             'total_revenue': total_revenue,
             'total_orders': len(orders),
         }
